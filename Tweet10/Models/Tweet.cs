0f0356a9bf9893a8039a29acd07b8c9d80aa1f9c
Jeffrey <Jeffrey@DESKTOP-PI13M22>
2022-01-29 19:06:09 +0100

    Add project files.

---
+++ Tweet10/Models/Tweet.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tweet10.Models
+{
+    public class Tweet
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Text { get; set; }
+        public DateTime createdDateTime { get; set; } = DateTime.Now;
+
+
+
+
+    }
+}
